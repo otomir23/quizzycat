9eba9d2e74f93a7346577084920f88ff2f773ac7
Damir Modyarov <otomir@yandex.ru>
2022-08-30 12:15:28 +0300

    Project setup

---
+++ components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export type LayoutProps = {
+    children: React.ReactNode;
+    title: string;
+}
+
+const Layout = ({children, title}: LayoutProps) => {
+    return (
+        <div className="flex flex-col min-h-screen items-center">
+            <Head>
+                <title>{title} | quizycat</title>
+            </Head>
+            <header className="my-12">
+                <Link href="/">
+                    <a className="font-bold text-4xl">quizycat</a>
+                </Link>
+            </header>
+            <main className="min-h-full w-full flex flex-col items-center">{children}</main>
+        </div>
+    );
+}
+
+export default Layout;
